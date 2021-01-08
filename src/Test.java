2d184a070eccc7c100ba6f4ed97b65db12d1a23a
18276030017 <2669255213@qq.com>
2021-01-08 20:25:40 +0800

    把程序修改为任意系统都可以读取Excel表格的数据

--- src/Test.java
+++ src/Test.java
@@ -1,33 +1,32 @@
-import java.io.File;
-import java.util.Scanner;
+    import java.io.File;
+    import java.io.InputStream;
+    import java.util.Scanner;
 
-public class Test {
-    public static void main(String[] args) {
-        /*
-        开始读文件
-         */
-        File file=new File("C:\\Users\\Administrator.PC-20190823HNEY\\Desktop\\ComdShop\\src\\CmdShop.xlsx");
-        ReadExcel readExcel = new ReadExcel();//创建对象
-        User users[] = readExcel.readExcel(file);//从表格中获取用户数据
+    public class Test {
+        public static void main(String[] args) throws ClassNotFoundException {
+            /*
+            开始读文件
+             */
+            //File file=new File("C:\\Users\\Administrator.PC-20190823HNEY\\Desktop\\ComdShop\\src\\CmdShop.xlsx");
+            InputStream in = Class.forName("Test").getResourceAsStream("/CmdShop.xlsx");
+            ReadExcel readExcel = new ReadExcel();//创建对象
+            User users[] = readExcel.readExcel(in);//从表格中获取用户数据
 
-        Scanner sc = new Scanner(System.in);//定义一个输入类
-        System.out.println( "请输入用户名：");
-        String username = sc.next();//获取输入的用户名
-        System.out.println("你的用户名是:"+  new String(username));
+            Scanner sc = new Scanner(System.in);//定义一个输入类
+            System.out.println( "请输入用户名：");
+            String username = sc.next();//获取输入的用户名
 
-        System.out.println("请输入密码:");
-        String password = sc.next();
-        System.out.println("你的密码是:"+  new String(password));
+            System.out.println("请输入密码:");
+            String password = sc.next();
 
-
-        for(int i=0;i<users.length;i++) {
-            if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
-                System.out.println("登录成功");
-                break;
-            } else {
-                System.out.println("登录失败");
+            for(int i=0;i<users.length;i++) {
+                if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
+                    System.out.println("登录成功");
+                    break;
+                } else {
+                    System.out.println("登录失败");
+                }
             }
         }
-    }
 
-}
+    }
